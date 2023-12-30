3df9d7a3634b9dcf0795d8d515f5a349a51b4ed3
curious-vv1 <vivekrealc2@gmail.com>
2023-12-30 18:33:35 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+
+export default function App() {
+  return (
+    <Home />
+  )
+}
+
